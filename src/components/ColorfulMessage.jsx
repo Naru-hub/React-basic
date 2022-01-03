6be68e9495ef85ad4107f22a4a27e2cfdd95c2f0
Nona <81672054+Naru-hub@users.noreply.github.com>
2022-01-04 00:48:26 +0900

    Initial commit

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ColofulMessage = (props) => {
+  const contentStyle = {
+    color: props.color,
+    fontsize: "18px"
+  };
+
+  return <p style={contentStyle}>{props.children}</p>;
+};
+
+export default ColofulMessage;
